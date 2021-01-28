408c4242062f5890f185128b9bb92bb5df599516
Jan Führer <jan_fue@icloud.com>
2021-01-28 11:55:04 +0100

    modified creature

--- Assets/Scripts/Bone.cs
+++ Assets/Scripts/Bone.cs
@@ -1,24 +1,59 @@
-using System;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Bone : MonoBehaviour
 {
-    public  GameObject musclePrefab;
-    public  Bone[]     muscleBones;
-    public  Bone[]     joints;
+    public GameObject musclePrefab;
+    public Bone[]     muscleBones;
+    public Bone[]     rightJoints;
+    public Bone[]     leftJoints;
 
     private Muscle[]   _muscles;
 
     private void Start()
     {
+        InitBones();
         InitMuscles();
     }
 
     private void InitBones()
     {
-        
+        foreach (var bone in rightJoints)
+            CreateHinge(bone, true);
+
+        foreach (var bone in leftJoints)
+            CreateHinge(bone, false);
+    }
+
+    private void CreateHinge(Bone bone, bool isRight)
+    {
+        var hinge = gameObject.AddComponent<HingeJoint2D>();
+        hinge.connectedBody                = bone.GetComponent<Rigidbody2D>();
+        hinge.autoConfigureConnectedAnchor = false;
+        hinge.useLimits                    = true;
+        var isMuscled = muscleBones.Any(bone.Equals);
+        if (isRight)
+        {
+            hinge.anchor          = new Vector2(2,  0);
+            hinge.connectedAnchor = new Vector2(-2, 0);
+            var isAbove = hinge.jointAngle > 0;
+            hinge.limits = new JointAngleLimits2D
+            {
+                max = isMuscled ? (isAbove ? 75 : 0)   : 75,
+                min = isMuscled ? (isAbove ? 0  : -75) : -75
+            };
+        }
+        else //isLeft
+        {
+            hinge.anchor          = new Vector2(-2, 0);
+            hinge.connectedAnchor = new Vector2(2,  0);
+            var isAbove = hinge.jointAngle < 0;
+            hinge.limits = new JointAngleLimits2D()
+            {
+                min = isMuscled ? (isAbove ? -75 : 75) : 75,
+                max = isMuscled ? 0 : -75
+            };
+        }
     }
 
     private void InitMuscles()
