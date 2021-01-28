ade78757d10bd2e1cdec82c5995cac02137c92e0
Jan Führer <jan_fue@icloud.com>
2021-01-28 11:15:01 +0100

    implemented muscles 2

---
+++ Assets/Scripts/Muscle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Muscle : MonoBehaviour
+{
+    public  Bone          fromBone;
+    public  Bone          toBone;
+    public  float         frequency;
+    private LineRenderer  _lineRenderer;
+    private SpringJoint2D _spring;
+
+    private void Start()
+    {
+        _lineRenderer                        = transform.GetComponent<LineRenderer>();
+        
+        _spring                              = fromBone.gameObject.AddComponent<SpringJoint2D>();
+        _spring.connectedBody                = toBone.GetComponent<Rigidbody2D>();
+        _spring.autoConfigureConnectedAnchor = false;
+        _spring.anchor                       = Vector2.zero;
+        _spring.connectedAnchor              = Vector2.zero;
+        _spring.frequency                    = frequency;
+        _spring.autoConfigureDistance        = false;
+    }
+
+    private void Update()
+    {
+        _lineRenderer.SetPosition(0, fromBone.transform.position);
+        _lineRenderer.SetPosition(1, toBone.transform.position);
+    }
+}
